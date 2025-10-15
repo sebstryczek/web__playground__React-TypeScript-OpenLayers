923647e31e3561c2c3edd9ccfe6ed314b5646e43
Sebastian <sebastian@stryczek.pl>
2025-10-15 14:10:16 +0200

    Add github pages deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/web__playground__React-TypeScript-OpenLayers/" : "/",
   server: {
     host: "0.0.0.0", // Needed for Docker
     port: 3000,
